83a9b291f805f7fd8db9641f96d8ee2a77018f2d
Adarsh Pandey <adarshpandey022@gmail.com>
2020-09-12 03:39:27 +0530

    almost complete

---
+++ javascript.js
@@ -0,0 +1,7 @@
+document.getElementById("pic1").addEventListener("click", function(){
+	document.querySelector(".popup").style.display = "flex";
+})
+
+document.getElementById("close").addEventListener("click", function(){
+	document.querySelector(".popup").style.display = "none";
+})
