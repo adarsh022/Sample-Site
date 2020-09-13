6e2b9ee766982f9ea2186cedd2dc6e89e51f6e74
Adarsh Pandey <adarshpandey022@gmail.com>
2020-09-13 18:00:58 +0530

    links fixed

--- javascript.js
+++ javascript.js
@@ -1,15 +1,33 @@
 document.getElementById("pic1").addEventListener("click", function(){
 	document.querySelector("#popup1").style.display = "flex";
 })
-
-document.getElementById("close1").addEventListener("click", function(){
-	document.querySelector("#popup1").style.display = "none";
+document.getElementById("pic2").addEventListener("click", function(){
+	document.querySelector("#popup1").style.display = "flex";
 })
-//--------------------------------------------------------------
+document.getElementById("pic3").addEventListener("click", function(){
+	document.querySelector("#popup1").style.display = "flex";
+})
+document.getElementById("pic4").addEventListener("click", function(){
+	document.querySelector("#popup1").style.display = "flex";
+})
+
+//----------------------------------------------------------------
 document.getElementById("vid1").addEventListener("click", function(){
 	document.querySelector("#popup2").style.display = "flex";
 })
-
+document.getElementById("vid2").addEventListener("click", function(){
+	document.querySelector("#popup2").style.display = "flex";
+})
+document.getElementById("vid3").addEventListener("click", function(){
+	document.querySelector("#popup2").style.display = "flex";
+})
+document.getElementById("vid4").addEventListener("click", function(){
+	document.querySelector("#popup2").style.display = "flex";
+})
+//----------------------------------------------------------------
+document.getElementById("close1").addEventListener("click", function(){
+	document.querySelector("#popup1").style.display = "none";
+})
 document.getElementById("close2").addEventListener("click", function(){
 	document.querySelector("#popup2").style.display = "none";
 })
